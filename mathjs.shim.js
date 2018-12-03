839919228178b0564d3162759dc353351b2187c7
jd <jean.daniel.michaud@gmail.com>
2018-12-03 18:37:33 +0100

    initial commit

---
+++ mathjs.shim.js
@@ -0,0 +1,53 @@
+window.f = [{
+  name: 'add',
+  fun: math.add,
+}, {
+  name: 'sub',
+  fun: math.subtract,
+}, {
+  name: 'mul',
+  fun: math.multiply,
+}, {
+  name: 'div',
+  fun: math.divide,
+}, {
+  name: 'norm',
+  fun: math.norm,
+}, {
+  name: 'transpose',
+  fun: math.transpose,
+}, {
+  name: 'dot',
+  fun: math.dot,
+}, {
+  name: 'reshape',
+  fun: math.reshape,
+}, {
+  name: 'cross',
+  fun: math.cross,
+}, {
+  name: 'inv',
+  fun: math.inv,
+}, {
+  name: 'normalize',
+  fun: function (v) {
+    const norm = math.norm(v);
+    return norm === 0 ? [0, 0, 0] : math.divide(v, norm);
+  }
+}, {
+  name: 'neg',
+  fun: function (v) {
+    return math.multiply(v, -1);
+  }
+}];
+
+window.f.forEach(({name, fun}) => {
+  if (Array.prototype[name] === undefined) {
+    Array.prototype[name] = function (arguments) {
+      if (arguments !== undefined) {
+        return fun(this, arguments);
+      }
+      return fun(this);
+    }
+  }
+});
